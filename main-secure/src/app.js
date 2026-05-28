5b15f98b07d64266eb9c5ca80ffb48b5e4c0b788
Mario <iriscarolina0589@gmail.com>
2026-05-28 03:05:48 -0400

    fix eslint errors in middleware

--- main-secure/src/app.js
+++ main-secure/src/app.js
@@ -31,13 +31,13 @@ app.get('/', (req, res) => {
 
 
 // 404 HANDLER 
-app.use((req, res) => {
+app.use((req, res, next) => {
     res.status(404).send("Página no encontrada");
 });
 
 
 // ERROR HANDLER
-app.use((err, req, res,next) => {
+app.use((err, req, res, next) => {
     console.error(err.message); // solo consola (backend)
 
     res.status(500).send("Algo salió mal. Intenta más tarde.");
